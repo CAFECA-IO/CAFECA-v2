17bb3a7ecdd3ef03db4cf649d3149909f6f40e3e
Julian <julian890701@gmail.com>
2022-11-22 17:01:12 +0800

    Add logo component

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Logo() {
+  return (
+    <Link to="/">
+      <img src="./cafeca/Group 68.svg" alt="CAFECA" />
+    </Link>
+  );
+}
+export default Logo;
