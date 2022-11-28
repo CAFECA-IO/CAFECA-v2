4ebb447ab5363aeeebf770c25ab680e0f6502bdf
Julian <julian890701@gmail.com>
2022-11-28 15:36:31 +0800

    Add footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import style from "./footer.module.css";
+
+function Footer() {
+  return (
+    <>
+      <div className={style.footer}>
+        <Link to="/" className={style.logo}>
+          <img src="./cafeca/cafeca_logo_mono.svg" alt="CAFECA" />
+        </Link>
+        <div className={style.copyright}>
+          Copyright © 2022 CAFECA. All rights reserved.
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
