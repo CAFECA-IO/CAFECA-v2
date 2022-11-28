9c91ac458a900ab17139fcb9dd4785d09da28541
Julian <julian890701@gmail.com>
2022-11-28 15:51:17 +0800

    Rename files

---
+++ src/components/menu/menu.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import Container from "react-bootstrap/Container";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import AboutUsPage from "../../pages/about_us_page";
+import HiringPage from "../../pages/hiring_page";
+import style from "./menu.module.css";
+
+function Menu() {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+  return (
+    <div className={style.container}>
+      {/*       <Navbar color="dark">
+        <NavbarBrand>CAFECA</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className={style.burger} />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav className={style.menu} navbar>
+            <NavItem>
+              <NavLink href="/about-us" element={<AboutUsPage />}>
+                About Us
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/hiring" element={<HiringPage />}>
+                Hiring
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar> */}
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/about-us">About Us</NavDropdown.Item>
+            <NavDropdown.Item href="/hiring">We Are Hiring</NavDropdown.Item>
+          </NavDropdown>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+export default Menu;
