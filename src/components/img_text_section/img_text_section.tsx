75f5936dfecf519424e0be378b223d031cb372e2
arealclimber <luffulgence@gmail.com>
2024-03-27 19:35:50 +0800

    feat: 🎸 show mobile-wise interface when width is below 1024

--- src/components/img_text_section/img_text_section.tsx
+++ src/components/img_text_section/img_text_section.tsx
@@ -3,22 +3,22 @@ import React from 'react';
 const ImgTextSection = () => {
   return (
     <div>
-      <div className="p-20 w-full max-md:px-5 max-md:max-w-full">
-        <div className="flex gap-5 max-md:flex-col-reverse max-md:gap-0">
-          <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+      <div className="p-20 w-full max-lg:px-5 max-lg:max-w-full">
+        <div className="flex gap-5 max-lg:flex-col-reverse max-lg:gap-0">
+          <div className="flex flex-col w-6/12 max-lg:ml-0 max-lg:w-full">
             <img
               loading="lazy"
               srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ea8901ae12d5f2305f993509f042e38f40f82c78c8ec6b914095e23800fa9ba1?apiKey=0e17b0b875f041659e186639705112b1&"
-              className="self-stretch my-auto w-full aspect-[1.49] max-md:mt-10 max-md:max-w-full"
+              className="self-stretch my-auto w-full aspect-[1.49] max-lg:mt-10 max-lg:max-w-full"
             />
           </div>
-          <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col grow justify-center py-8 max-md:mt-10 max-md:max-w-full">
-              <div className="flex flex-col max-md:max-w-full">
-                <div className="justify-center text-4xl font-bold leading-10 text-gray900 max-md:max-w-full">
+          <div className="flex flex-col ml-5 w-6/12 max-lg:ml-0 max-lg:w-full">
+            <div className="flex flex-col grow justify-center py-8 max-lg:mt-10 max-lg:max-w-full">
+              <div className="flex flex-col max-lg:max-w-full">
+                <div className="justify-center text-4xl font-bold leading-10 text-gray900 max-lg:max-w-full">
                   <span className="text-primaryPurple">Simple</span> Way To Prove Who You Are
                 </div>
-                <div className="mt-12 text-lg leading-6 text-gray-500 max-md:mt-10 max-md:max-w-full">
+                <div className="mt-12 text-lg leading-6 text-gray-500 max-lg:mt-10 max-lg:max-w-full">
                   <span className="font-bold">Digital Resident Identity:</span> An identity
                   verification within or outside Palau allows for extension of visa duration up to
                   180 days.
--- src/components/text_img_section/text_img_section.tsx
+++ src/components/text_img_section/text_img_section.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const TextImgSection = () => {
   return (
     <div>
-      <div className="p-20 w-full max-md:px-5 max-md:max-w-full">
-        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          <div className="flex flex-col w-[43%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col justify-center self-stretch py-8 my-auto font-bold max-md:mt-10 max-md:max-w-full">
-              <div className="flex flex-col max-md:max-w-full">
-                <div className="justify-center text-5xl leading-[52.8px] text-gray900 max-md:max-w-full max-md:text-4xl max-md:leading-[49px]">
+      <div className="p-20 w-full max-lg:px-5 max-lg:max-w-full">
+        <div className="flex gap-5 max-lg:flex-col max-lg:gap-0">
+          <div className="flex flex-col w-[43%] max-lg:ml-0 max-lg:w-full">
+            <div className="flex flex-col justify-center self-stretch py-8 my-auto font-bold max-lg:mt-10 max-lg:max-w-full">
+              <div className="flex flex-col max-lg:max-w-full">
+                <div className="justify-center text-5xl leading-[52.8px] text-gray900 max-lg:max-w-full max-lg:text-4xl max-lg:leading-[49px]">
                   <span className="text-primaryPurple">One-Time</span> KYC Only
                 </div>
-                <div className="mt-12 text-lg leading-6 text-gray-500 max-md:mt-10 max-md:max-w-full">
+                <div className="mt-12 text-lg leading-6 text-gray-500 max-lg:mt-10 max-lg:max-w-full">
                   <span className="">Step 1:</span> Upload ID <br />
                   <span className="">Step 2:</span> Verify personal info
                   <br />
@@ -24,11 +24,11 @@ const TextImgSection = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col ml-5 w-[57%] max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col ml-5 w-[57%] max-lg:ml-0 max-lg:w-full">
             <img
               loading="lazy"
               srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/0a1ef506f3be7acc31c3a3fb998d5f5a44752ad28705cc13bbf78173ae737bc3?apiKey=0e17b0b875f041659e186639705112b1&"
-              className="self-stretch my-auto w-full aspect-[1.75] max-md:mt-10 max-md:max-w-full"
+              className="self-stretch my-auto w-full aspect-[1.75] max-lg:mt-10 max-lg:max-w-full"
             />
           </div>
         </div>
