8747bcb247f7d9f8123fd7b150d4a6ab7a8734a3
arealclimber <luffulgence@gmail.com>
2024-03-28 20:28:24 +0800

    feat: 🎸 svg animation works well
    ✅ Closes: #18 #19

--- src/components/contact_form_section/contact_form_section.tsx
+++ src/components/contact_form_section/contact_form_section.tsx
@@ -5,16 +5,18 @@ import ContactForm from '../contact_form/contact_form';
 const ContactFormSection = () => {
   return (
     <div id="contact-us" className="relative h-1440px">
+      <div className="absolute z-10 h-full w-full bg-contact_bg bg-cover bg-no-repeat"></div>
+
       <div className="absolute top-0 h-500px w-full">
         {' '}
-        <Image
+        {/* <Image
           loading="lazy"
           src="/animations/contact_bg.svg"
           alt="contact_bg"
           fill
-          style={{objectFit: 'cover'}}
+          style={{objectFit: 'contain'}}
           className="z-10"
-        />
+        /> */}
         <div className="pt-0 flex flex-col w-full max-md:max-w-full">
           <div className="flex overflow-hidden relative flex-col justify-center items-center px-16 pt-16 pb-0 w-full min-h-[1440px] max-md:px-5 max-md:max-w-full">
             {/* <div className="relative h-500px w-full">
