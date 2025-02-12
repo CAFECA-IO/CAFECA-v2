74e494f7b265d0f219208eb9f8f9a9a7cd7ac1c2
Luphia Chang <luphia@cafeca.io>
2025-02-12 19:12:36 +0800

    update config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,5 @@
-export const PHONE_NUMBER = '+1 (647) 123-4567';
-export const EMAIL = 'info@company.com';
-export const ADDRESS = '1234 Street, City, Province, Postal Code';
-export const COPYRIGHT = 'CAFECA Inc. @ 2024. All rights reserved.';
+const CURRENT_YEAR = new Date().getFullYear();
+export const COPYRIGHT = `CAFECA Inc. @ ${CURRENT_YEAR}. All rights reserved.`;
 
 export const CAFECA_URL = {
   HOME: '/',
